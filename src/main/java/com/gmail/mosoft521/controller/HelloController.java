972f1edb2cab71416bea49d9360e124d536fe1b8
Alvin Zhang <mosoft521@gmail.com>
2018-06-13 07:39:39 +0800

    加log控制

--- src/main/java/com/gmail/mosoft521/controller/HelloController.java
+++ src/main/java/com/gmail/mosoft521/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.gmail.mosoft521.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @EnableAutoConfiguration
 public class HelloController {
+    private static Logger LOGGER = LoggerFactory.getLogger(HelloController.class);
+
     public static void main(String[] args) throws Exception {
         SpringApplication.run(HelloController.class, args);
         //运行之后在浏览器中访问：http://localhost:8080/hello
@@ -17,6 +21,7 @@ public class HelloController {
     @RequestMapping("/hello")
     @ResponseBody
     String home() {
+        LOGGER.info("Hello world!");
         return "Hello ,spring boot!";
     }
 }
