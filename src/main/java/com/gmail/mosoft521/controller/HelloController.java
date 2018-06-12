cc2be4d4c273ddc561c8885e7a9856a7c3cb02ec
Alvin Zhang <mosoft521@gmail.com>
2018-06-13 06:57:17 +0800

    init:war包可以在tomcat8下启动，访问http://localhost:8080/hello

---
+++ src/main/java/com/gmail/mosoft521/controller/HelloController.java
@@ -0,0 +1,22 @@
+package com.gmail.mosoft521.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@EnableAutoConfiguration
+public class HelloController {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(HelloController.class, args);
+        //运行之后在浏览器中访问：http://localhost:8080/hello
+    }
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    String home() {
+        return "Hello ,spring boot!";
+    }
+}
---
+++ src/test/java/com/gmail/mosoft521/AppTest.java
@@ -0,0 +1,18 @@
+package com.gmail.mosoft521;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
